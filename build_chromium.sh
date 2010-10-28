94dffe50a6a7d28af9ae35669afceba231524bbe
Yakkala Yagnesh Raghava <yagneshmsc@gmail.com>
2010-10-28 23:09:52 +0900

    adding build_chromimu.sh and softbuild.pl

---
+++ build_chromium.sh
@@ -0,0 +1,9 @@
+sudo add-apt-repository ppa:gwibber-daily/ppa && sudo apt-get update &&
+sudo apt-get install chromium-browser
+
+&& echo '************************' \
+chrome is now installed \
+'******************************'
+
+	
+
---
+++ softbuild.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/env perl
+#---------------------------------------------------------------------------
+# Author: yagnesh raghava yakkala  Email: yagneshmsc@NOSPAMgmail.com
+# DATE:
+# Purpose:
+#
+#
+# Current Version: 0.1
+# Latest change by on
+#---------------------------------------------------------------------------
+
+use strict;
+use warnings;
