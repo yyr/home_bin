08527d18549780e8a423e0473f7d670faff84811
Yakkala Yagnesh Raghava <yagneshmsc@gmail.com>
2011-01-26 01:25:49 +0900

    emacsd init file added

--- build_chromium.sh
+++
@@ -1,9 +0,0 @@
-sudo add-apt-repository ppa:gwibber-daily/ppa && sudo apt-get update &&
-sudo apt-get install chromium-browser
-
-&& echo '************************' \
-chrome is now installed \
-'******************************'
-
-	
-
