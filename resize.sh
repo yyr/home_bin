88d6d39ea452b5f17004ec95010dfc420de2a2b8
Yakkala Yagnesh Raghava <yagneshmsc@gmail.com>
2011-02-15 02:51:08 +0900

    resize.sh: almost final

--- resize.sh
+++ resize.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 #any image file will be converted to thumb nail size
 
-function usage() {
+function usage() {		# tell the usage
         echo USAGE: "$1 <web/ppt> <file name> "
 }
 
-function split-path () {
-fullpath=$@
-
+function split-path () {	# split the path of a file
+    fullpath=$@
     filename="${fullpath##*/}"                      # Strip longest match of */ from start
     dir="${fullpath:0:${#fullpath} - ${#filename}}" # Substring from 0 thru pos of filename
     base="${filename%.[^.]*}"                       # Strip shortest match of . plus at least one non-dot char from end
@@ -16,11 +15,50 @@ fullpath=$@
         base=".$ext"
         ext=""
     fi
+}
+
+function convert-this-file() {	# convert to desired format
+    cd ${dir:-.}
+    case $1 in
+	web)
+	    if [ -f ${base}.${ext} ]; then
+		echo "creating web thumb for ${filename}"
+		convert -thumbnail 250 "${base}.${ext}"  "${base}.websize..${ext}"
+		echo " created ${base}.websize.${ext}"
+	    else
+		echo "'$1' is not a valid file"    
+	    fi
+	    ;;
+
+	ppt)
+	    if [ -f ${base}.${ext} ]; then
+		echo "creating ppt size for ${filename}"
+		convert -thumbnail 200 "${base}.${ext}"  "${base}.pptsize.${ext}"
+		echo " created ${base}.pptsize.${ext}"
+	    else
+		echo "'$1' is not a valid file"    
+	    fi
+	    ;;
+	hui*)
+	    if [ -f ${base}.${ext} ]; then
+		echo "creating Thumbnail for ${filename}"
+		convert -thumbnail 500 "${base}.${ext}"  "${base}.huisa.${IMG}"
+		echo " created ${base}.huisa.${IMG}"
+	    else
+		echo "'$1' is not a valid file"    
+	    fi
+	    ;;
+	* )
+	    usage $0
+	    exit $WRONG_OPTION
+	    ;;
+    esac
 
 }
 
 MINARGS=2
 E_ARGCOUNT=65
+WRONG_OPTION=420
 
 if [ $# -lt $MINARGS ]
 then
@@ -31,43 +69,13 @@ fi
 
 arg1=$1;			# option
 shift;
-arg2=$@				# filename
-JPG-TYPE=jpg			# if output jpg
+in_files=$@				# filename
+cwd=`pwd`
+IMG=jpg			# for specific image type
 
-split-path $arg2
-cd ${dir:-.}
-pwd
 
-case $arg1 in
-    web)
-	if [ -f ${base}.${ext} ]; then
-	    echo "creating web thumb for ${filename}"
-	    convert -thumbnail 250 ${base}.${ext}  ${base}.thumb.${ext}
-	    echo " created ${base}.websize.${ext}"
-	else
-	    echo "'$1' is not a valid file"    
-	fi
-	;;
+for file in $in_files; do
+    split-path $file
+    convert-this-file $arg1
+done
 
-    ppt)
-	if [ -f ${base}.${ext} ]; then
-	    echo "creating ppt size for ${filename}"
-	    convert -thumbnail 200 "${base}.${ext}"  "${base}.pptsize.${ext}"
-	    echo " created ${base}.pptsize.${ext}"
-	else
-	    echo "'$1' is not a valid file"    
-	fi
-	;;
-    hui*)
-	if [ -f ${base}.${ext} ]; then
-	    echo "creating Thumbnail for ${filename}"
-	    convert -thumbnail 500 "${base}.${ext}"  "${base}.pptsize.${JPG-TYPE}"
-	    echo " created ${base}.huisa.${JPG-TYPE}"
-	else
-	    echo "'$1' is not a valid file"    
-	fi
-    *)
-    usage $0
-    ;;
-    
-esac
