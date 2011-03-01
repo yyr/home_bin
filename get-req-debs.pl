a06e91eb246a190acd9b927edffa0ea7ec350927
Yakkala Yagnesh Raghava <yagneshmsc@gmail.com>
2011-03-01 17:58:40 +0900

    del: old script deletion

---
+++ get-req-debs.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/env perl
+#
+# Copyright (C) Yagnesh Raghava Yakkala. www.yagnesh.org
+#    File: (place (buffer-name))
+#  Author: Yagnesh Raghava Yakkala <yagnesh@live.com>
+# Created: Thursday, February 24 2011
+#
+# Description:
+use strict;
+use warnings;
+
+my packs = {
+	    texlive,
+	    texlive-base,
+	    texlive-latex-extra,
+	    texi2html,
+	    git,
+	    gitk,
+	    subversion,
+	    build-essential,
+	    autoconf,
+	   };
+
+sub function_name {
+  
+}
