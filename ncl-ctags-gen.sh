a796dc5fa32d5fd6640cd0f72f8c2e37e7eeca90
Yakkala Yagnesh Raghava <yagnesh@live.com>
2011-12-09 22:54:38 +0900

    * ncl-ctags-gen.sh: improved regular expression

--- ncl-ctags-gen.sh
+++ ncl-ctags-gen.sh
@@ -10,7 +10,7 @@
 # Description:
 
 ctags-exuberant -e -a --verbose=yes  --langdef=ncl \
-    --langmap=ncl:.ncl --regex-ncl='/^[[:space:]]*function[[:space:]]+(.*)/\1/f,function/' \
-    --regex-ncl='/^[[:space:]]*procedure[[:space:]]+(.*)/\1/p,procedure/' `find ${1:-"."} -type f -name "*.ncl"`
+    --langmap=ncl:.ncl --regex-ncl='/^[[:space:]]*function[[:space:]]+([a-zA-Z0-9_]+)[:blank:]*.*/\1/f,function/' \
+    --regex-ncl='/^[[:space:]]*procedure[[:space:]]+([a-zA-Z0-9_]+)[:blank:].*/\1/p,procedure/' `find ${1:-"."} -type f -name "*.ncl"`
 
 # ncl-ctags-gen.sh ends here
