35c31dbb004bd4c9170f606778d0d5c9fd5e03ae
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2014-04-16 13:46:29 +0900

    Easy one to open journal links.

---
+++ jopen.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Created: Wednesday, April 16 2014
+
+links=(http://journals.ametsoc.org/loi/bams
+       http://journals.ametsoc.org/loi/atsc
+       http://journals.ametsoc.org/loi/mwre
+       http://journals.ametsoc.org/toc/wefo
+       http://journals.ametsoc.org/loi/clim
+       http://www.sciencedirect.com/science/journal/01698095
+       http://preview.onlinelibrary.wiley.com/agu/journal/10.1002/%28ISSN%292169-8996/
+       https://www.jstage.jst.go.jp/browse/jmsj
+       http://www.atmos-chem-phys.net/volumes_and_issues.html
+       http://link.springer.com/journal/382)
+
+count=${#links[@]}
+index=0
+while [ "$index" -lt "$count" ]; do
+    echo -e "$index: ${links[$index]}"
+    let "index++"
+done
+echo "-1: forall links    <----"
+
+echo  "Select #:"
+read ans
+
+if [ "$ans" -eq "$ans" ]; then
+    if [ "$ans" -eq -1 ]; then
+        for link in ${links[@]}; do
+            firefox -url $link
+        done
+    else
+        firefox -url ${links[$ans]}
+    fi
+fi
