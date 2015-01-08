ac13d71d3c23e05606a06bcadac0846b6bd57a6b
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2015-01-08 18:08:08 +0900

    * mirror.sh: log time taken for backup

--- mirror.sh
+++ mirror.sh
@@ -2,9 +2,12 @@
 
 log_file="/var/log/mirror_backup.log"
 
-echo "Nightly Backup Started: $(date)" >> $log_file
+t="$(date +%s)"
+echo "$(date "+%F %H:%M:%S"): Mirroring Started." >> $log_file
 
 rsync -azXS --exclude=.gvfs --delete /home/ /backup/home/
 rsync -azXS --delete /dump/ /backup/dump/
 
-echo "Nightly Backup Successful: $(date)" >> $log_file
+t="$(($(date +%s)-t))"
+printf "$(date "+%F %H:%M:%S"): Mirroring Finished. \nElapsed time: %02d:%02d:%02d\n" \
+       "$((t/3600))" "$((t%3600/60))"  "$((t%60))"
