63208ff14afde492c09700f54a28c7f1c202a4bb
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2015-01-15 15:01:54 +0900

    * getaudio.sh: fix usage bit

--- getaudio.sh
+++ getaudio.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Created: Thursday, January 15 2015
-# Usage: getaudio.sh in.mp4 out.mp4
+# Usage: getaudio.sh 1.mp4 [2.mp4 ... 10.mp4]
 
 for f in "$@"; do
     avconv -i "$f" -c copy -map 0:a "$f".mp4
