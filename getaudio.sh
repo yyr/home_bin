e586483e219c61ac92fabe3790dff5932594c25c
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2015-01-15 14:59:50 +0900

    * getaudio.sh: take multiple args.

--- getaudio.sh
+++ getaudio.sh
@@ -2,4 +2,6 @@
 # Created: Thursday, January 15 2015
 # Usage: getaudio.sh in.mp4 out.mp4
 
-avconv -i "$1" -c copy -map 0:a "$2"
+for f in "$@"; do
+    avconv -i "$f" -c copy -map 0:a "$f".mp4
+done
