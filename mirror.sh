573e0bca7cd1d7a51765948f09a5c10dcbd26b7c
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2015-01-15 09:30:17 +0900

    * mirror.sh: minor

--- mirror.sh
+++ mirror.sh
@@ -3,11 +3,11 @@
 log_file="/var/log/mirror_backup.log"
 
 t="$(date +%s)"
-echo "$(date "+%F %H:%M:%S"): Mirroring Started." >> $log_file
+printf "$(date +"%F"): Mirroring started at $(date +"%r") --> " >> $log_file
 
 rsync -azXS --exclude=.gvfs --delete /home/ /backup/home/
 rsync -azXS --delete /dump/ /backup/dump/
 
 t="$(($(date +%s)-t))"
-printf "$(date "+%F %H:%M:%S"): Mirroring Finished. \nElapsed time: %02d:%02d:%02d\n" \
-       "$((t/3600))" "$((t%3600/60))"  "$((t%60))"
+printf "Finished at $(date +"%r"). Elapsed time: %02d:%02d:%02d\n" \
+       "$((t/3600))" "$((t%3600/60))"  "$((t%60))" >> $log_file
