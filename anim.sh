018a6f88dcfb8af77d29f53319ccbb0485f2840a
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2015-01-18 12:33:25 +0900

    * anim.sh:  use avconv to make animations not convert.

--- anim.sh
+++ anim.sh
@@ -2,6 +2,24 @@
 #    File: anim.sh
 # Created: Wednesday, May 11 2011
 
-convert -verbose -delay 41 -loop 0 -density 50 $@
+# link
+base="${1%.[^.]*}"
+ext="${1:${#base} + 1}"
 
+echo base=$base , ext=$ext
+
+rm -i out.mp4
+
+x=1;
+for i in $@;
+do
+    counter=$(printf %04d $x);
+    ln -sf $(pwd)/"$i" /tmp/img"$counter"."$ext";
+    x=$(($x+1));
+done
+
+# make movie
+avconv -f image2 -i /tmp/img%03d.jpg out.mp4
+
+rm /tmp/img*.$ext
 # anim.sh ends here
