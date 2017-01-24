b36a5dce1819ce0487ad26d66bb073d495e3c9bb
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2017-01-24 15:50:19 +0530

    * jopen.sh: fix link

--- jopen.sh
+++ jopen.sh
@@ -7,7 +7,7 @@ links=(http://journals.ametsoc.org/loi/bams
        http://journals.ametsoc.org/toc/wefo/current
        http://journals.ametsoc.org/loi/clim
        http://www.sciencedirect.com/science/journal/01698095
-       http://preview.onlinelibrary.wiley.com/agu/journal/10.1002/%28ISSN%292169-8996/
+       http://agupubs.onlinelibrary.wiley.com/hub/jgr/journal/10.1002/%28ISSN%292169-8996/
        https://www.jstage.jst.go.jp/browse/jmsj
        http://www.atmos-chem-phys.net/volumes_and_issues.html
        http://link.springer.com/journal/382)
